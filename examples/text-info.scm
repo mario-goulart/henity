cfac56d96847e7dc978c56b5b7f81e61e13d95c1
Mario Domenech Goulart <mario@parenteses.org>
2021-05-12 19:53:29 +0200

    examples/text-info.scm: typo fix

--- examples/text-info.scm
+++ examples/text-info.scm
@@ -7,7 +7,7 @@
   (lambda ()
     (print "This is the legalese.")))
 
-(print (if (text-info-dialog '((title="License")
+(print (if (text-info-dialog '((title "License")
                                (filename "COPYING")
                                (checkbox "I read and accept the terms.")))
            "Start installation!"
